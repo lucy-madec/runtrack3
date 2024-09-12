ef723fd22f7ff7d731adefbe42d4b55dd454707d
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-09-12 16:11:55 +0200

    Took off the 'else' bloc

--- jour05/job01/db.php
+++ jour05/job01/db.php
@@ -7,12 +7,7 @@ $pass = '';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie.";
 } catch (PDOException $e) {
     die('Erreur de connexion : ' . $e->getMessage());
-}
-
-if ($pdo) {
-    echo "Connexion réussie.";
-} else {
-    echo "Erreur de connexion.";
 }
