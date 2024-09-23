648fb91bf6008a134f6410ad8920577a5bc88861
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-09-23 10:54:09 +0200

    Added a text input for the first name and last name with the class 'active' for the labels

--- jour07/job01/script.js
+++ jour07/job01/script.js
@@ -1,4 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+// Activate the select options in form
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
+});
+
+// Prefilling text inputs
+$(document).ready(function () {
+    M.updateTextFields();
 });
